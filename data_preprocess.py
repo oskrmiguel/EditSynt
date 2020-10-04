334655f5fcc08379597349e63eae155b4e8c5ddb
Aitor Soroa <a.soroa@gmail.com>
2020-10-04 08:41:33 +0200

    Add dependency parser output when preprocessing

--- data_preprocess.py
+++ data_preprocess.py
@@ -8,7 +8,7 @@ from label_edits import sent2edit
 
 # This script contains the reimplementation of the pre-process steps of the dataset
 # For the editNTS system to run, the dataset need to be in a pandas DataFrame format
-# with columns ['comp_tokens', 'simp_tokens','comp_ids','simp_ids', 'comp_pos_tags', 'comp_pos_ids', edit_labels','new_edit_ids']
+# with columns ['comp_tokens', 'simp_tokens','comp_ids','simp_ids', 'comp_pos_tags', 'comp_pos_ids', edit_labels','new_edit_ids', 'comp_dep_tree]
 
 PAD = 'PAD' #  This has a vocab id, which is used to represent out-of-vocabulary words [0]
 UNK = 'UNK' #  This has a vocab id, which is used to represent out-of-vocabulary words [1]
@@ -17,14 +17,34 @@ DEL = 'DEL' # This has a vocab id, which is used for deleting the corresponding 
 START = 'START' # this has a vocab id, which is uded for indicating start of the sentence for decoding [4]
 STOP = 'STOP' # This has a vocab id, which is used to stop decoding [5]
 
-class SpacyTag:
-    def __init__(self, model = 'en'):
-        self.nlp = spacy.load(model)
+class Spacy:
+    def __init__(self, lang):
+        l2m = { 'en' : 'en_core_web_sm' }
+        if lang not in l2m:
+            print('Language "{}" not supported.'.format(lang))
+            exit(1)
+        self.nlp = spacy.load(l2m[lang])
         self.tokenizer = self.nlp.Defaults.create_tokenizer(self.nlp)
-    def pos_tag(self, sent):
-        return [(token.text, token.pos_) for token in self.nlp(" ".join(sent))]
+
+    def analize(self, sent):
+        return self.nlp(" ".join(sent))
+
     def tokenize(self, sent):
-        return [token.text for token in self.tokenizer(" ".join(sent))]
+        if type(sent) == list:
+            sent = " ".join(sent)
+        return [token.text for token in self.tokenizer(sent)]
+
+    def _tree2str(self, root):
+        str = ''
+        for child in root.children:
+            str += '({}:{}'.format(child.dep_, child.text) + self._tree2str(child)
+        str += ')'
+        return str
+
+    def tree2str(self, doc):
+        'Create a parse string from a analyzed sentence'
+        root = [token for token in doc if token.head == token][0]
+        return '(root:{}'.format(root.text) + self._tree2str(root) + ')'
 
 def remove_lrb(sent_string):
     # sent_string = sent_string.lower()
@@ -45,7 +65,7 @@ def replace_lrb(sent_string):
     return new_sent
 
 
-def process_raw_data(comp_txt, simp_txt, pos_vocab):
+def process_raw_data(comp_txt, simp_txt, pos_vocab, lang):
     comp_txt = [line.lower().split() for line in comp_txt]
     simp_txt = [line.lower().split() for line in simp_txt]
     comp_txt,simp_txt=zip(*[(i[0],i[1]) for i in zip(comp_txt,simp_txt) if i[0] != i[1]])
@@ -68,19 +88,24 @@ def process_raw_data(comp_txt, simp_txt, pos_vocab):
         df['edit_labels'] = edits_list
         return df
 
-    def add_pos(df, src_sentences, pos_vocab, spacy):
-        print("POS tagging:", end='', flush = True)
+    def add_pos_dep(df, src_sentences, pos_vocab, spacy):
+        print("POS and DEP tagging:", end='', flush = True)
         comp_sentences = []
         pos_sentences = []
+        dep_sentences = []
         for i, sent in enumerate(src_sentences):
             if not i % 10000:
                 print(' {}'.format(i), end=' ', flush = True)
-            pos_sentence = spacy.pos_tag(sent)
+            spacy_doc = spacy.analize(sent)
+            pos_sentence = [(token.text, token.pos_) for token in spacy_doc]
+            tree_str = spacy.tree2str(spacy_doc)
             comp_sentences.append([x[0] for x in pos_sentence])
             pos_sentences.append(pos_sentence)
+            dep_sentences.append(tree_str)
         print('done')
         df['comp_tokens'] = comp_sentences
         df['comp_pos_tags'] = pos_sentences
+        df['comp_dep_tree'] = dep_sentences
         pos_ids_list = []
         for i,psent in enumerate(pos_sentences):
             pos_ids = [pos_vocab.w2i[w[1]] if w[1] in pos_vocab.w2i.keys() else pos_vocab.w2i[UNK] for w in psent]
@@ -88,21 +113,21 @@ def process_raw_data(comp_txt, simp_txt, pos_vocab):
         df['comp_pos_ids'] = pos_ids_list
         return df
 
-    spacy = SpacyTag()
+    spacy = Spacy(lang)
     df['simp_tokens'] = [spacy.tokenize(x) for x in simp_txt]
-    df = add_pos(df, comp_txt, pos_vocab, spacy)
+    df = add_pos_dep(df, comp_txt, pos_vocab, spacy)
     df = add_edits(df)
     return df
 
 def editnet_data_to_editnetID(df,vocab, output_path):
     """
-    this function reads from df.columns=['comp_tokens', 'simp_tokens', 'edit_labels','comp_pos_tags','comp_pos_ids']
+    this function reads from df.columns=['comp_tokens', 'simp_tokens', 'edit_labels','comp_pos_tags','comp_pos_ids', 'comp_dep_tree']
     and add vocab ids for comp_tokens, simp_tokens, and edit_labels
-    :param df: df.columns=['comp_tokens', 'simp_tokens', 'edit_labels','comp_pos_tags','comp_pos_ids']
+    :param df: df.columns=['comp_tokens', 'simp_tokens', 'edit_labels','comp_pos_tags','comp_pos_ids', 'comp_dep_tree']
     :param output_path: the path to store the df
     :return: a dataframe with df.columns=['comp_tokens', 'simp_tokens', 'edit_labels',
                                             'comp_ids','simp_id','edit_ids',
-                                            'comp_pos_tags','comp_pos_ids'])
+                                            'comp_pos_tags','comp_pos_ids', 'comp_dep_tree'])
     """
     out_list = []
 
@@ -121,13 +146,14 @@ def editnet_data_to_editnetID(df,vocab, output_path):
     for i,example in df.iterrows():
         comp_id, simp_id, edit_id = prepare_example(example,vocab)
         ex=[example['comp_tokens'], comp_id,
-         example['simp_tokens'], simp_id,
-         example['edit_labels'], edit_id,
-         example['comp_pos_tags'],example['comp_pos_ids']
+            example['simp_tokens'], simp_id,
+            example['edit_labels'], edit_id,
+            example['comp_pos_tags'],example['comp_pos_ids'],
+            example['comp_dep_tree']
          ]
         out_list.append(ex)
     outdf = pd.DataFrame(out_list, columns=['comp_tokens','comp_ids', 'simp_tokens','simp_ids',
-                                            'edit_labels','new_edit_ids','comp_pos_tags','comp_pos_ids'])
+                                            'edit_labels','new_edit_ids','comp_pos_tags','comp_pos_ids','comp_dep_tree'])
     outdf.to_pickle(output_path)
     print('saved to %s'%output_path)
     return outdf
--- do_preprocess_wikilargemartin.py
+++ do_preprocess_wikilargemartin.py
@@ -8,37 +8,46 @@ import pandas as pd
 
 random.seed(233)
 
-def build_vocab(df, out):
+
+def build_vocab(files, out, lang):
     V = {}
-    for k in ['simp_tokens', 'comp_tokens']:
-        for w in df[k]:
-            V[w] = V.get(w, 0) + 1
+    F = []
+    spacy = dp.Spacy(lang)
+    for x in files:
+        F.append(x[1])
+        F.append(x[2])
+    for fname in F:
+        for line in open(fname):
+            for w in spacy.tokenize(line.rstrip().lower()):
+                V[w] = V.get(w, 0) + 1
     fo = open(out, 'w')
     for k, v in sorted(V.items(), key=lambda x: x[1], reverse=True):
         fo.write("{} {}\n".format(k, v))
 
-def do_file(c_in, s_in, pos_vocab, outfile):
+def do_file(c_in, s_in, word_vocab, pos_vocab, outfile, lang):
     complex_fh=open(c_in)
     simple_fh=open(s_in)
-    df = dp.process_raw_data(complex_fh, simple_fh, pos_vocab)
-    build_vocab(df, os.path.join(out, "vocab.txt"))
-    word_vocab = vocabulary.Vocab()
-    word_vocab.add_vocab_from_file(os.path.join(out,"vocab.txt"), 30000)
+    df = dp.process_raw_data(complex_fh, simple_fh, pos_vocab, lang)
     return dp.editnet_data_to_editnetID(df, word_vocab, outfile)
-
 
 files = [['test', '/sc01a7/sisx09/sx09a1/jirhizts/Corpus/SimplificationDatasets/wikilarge-martin_split/wikilarge/wiki.full.aner.ori.test.src', '/sc01a7/sisx09/sx09a1/jirhizts/Corpus/SimplificationDatasets/wikilarge-martin_split/wikilarge/wiki.full.aner.ori.test.dst'],
 ['train', '/sc01a7/sisx09/sx09a1/jirhizts/Corpus/SimplificationDatasets/wikilarge-martin_split/wikilarge/wiki.full.aner.ori.train.src', '/sc01a7/sisx09/sx09a1/jirhizts/Corpus/SimplificationDatasets/wikilarge-martin_split/wikilarge/wiki.full.aner.ori.train.dst'],
 ['dev', '/sc01a7/sisx09/sx09a1/jirhizts/Corpus/SimplificationDatasets/wikilarge-martin_split/wikilarge/wiki.full.aner.ori.valid.src', '/sc01a7/sisx09/sx09a1/jirhizts/Corpus/SimplificationDatasets/wikilarge-martin_split/wikilarge/wiki.full.aner.ori.valid.dst']]
 
-out ='data/wikilarge'
+#files = [['all', 't1', 't2']]
 
-#build_vocab(files, os.path.join(out,"vocab.txt"))
+out ='data/wikilarge_dep'
+vocab_file = os.path.join(out,"vocab.txt")
+
+lang='en'
+build_vocab(files, vocab_file, lang)
+word_vocab = vocabulary.Vocab()
+word_vocab.add_vocab_from_file(vocab_file, 30000)
 pos_vocab = vocabulary.POSvocab('vocab_data/ptb_ud_tagset.txt')
 
 for split,c,s in files:
     print('Processing ', split)
-    do_file(c, s, word_vocab, pos_vocab, os.path.join(out, "{}.df.filtered.pos".format(split)))
+    do_file(c, s, word_vocab, pos_vocab, os.path.join(out, "{}.df.filtered.dep".format(split)), lang)
 
 # split 90% train, 10% val
 # train=df.sample(frac=0.9,random_state=233) #random state is a seed value
